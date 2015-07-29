709c47e98d6834cf6e56c08754639ff5aac579e7
TravisCarden <travis.carden@gmail.com>
2015-07-28 21:46:31 -0500

    Added xfce screen layouts.

---
+++ home/.screenlayout/laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output LVDS --mode 1280x800 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output VGA-0 --off --output DisplayPort-0 --off
---
+++ home/.screenlayout/three-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --mode 1680x1050 --pos 0x0 --rotate normal --output LVDS --mode 1280x854 --pos 3360x196 --rotate normal --output DisplayPort-2 --mode 1680x1050 --pos 1680x0 --rotate normal --output VGA-0 --off --output DisplayPort-0 --off
